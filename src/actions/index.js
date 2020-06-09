60ca0107b7d19b535d2ef8d55234cb51a6e92461
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-06-09 13:23:11 -0400

    memoized fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
    const { data } = await jsonPlaceholder.get('/posts');
    dispatch({ type: 'FETCH_POSTS', payload: data });
 }
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// only fetching the user one time with memoize function
+const _fetchUser = _.memoize(async (id, dispatch) => {
    const { data } = await jsonPlaceholder.get(`/users/${id}`);
    dispatch({ type: 'FETCH_USER', payload: data });
-}
+});

b864114f63b16f9c035f856d40e98bda8cb2c109
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-06-09 12:41:14 -0400

    moved users.find logic into mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,10 +7,10 @@ class UserHeader extends Component {
       this.props.fetchUser(this.props.userId);
    }
    render() {
-      const user = this.props.users.find(user => user.id === this.props.userId);
+      const { user } = this.props;
       if(!user) return null;
       return <div className='header'>{user.name}</div>;
    }
 }
-const mapStateToProps = state => ({ users: state.users });
+const mapStateToProps = (state, ownProps) => ({ user: state.users.find(user => user.id === ownProps.userId) });
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
